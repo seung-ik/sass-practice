631ebe4dd7a115d23aa0b285c95cccdfb6b58a31
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-06-04 17:16:50 +0900

    typscript enum

---
+++ pr_ts.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var ExEnum;
+(function (ExEnum) {
+    ExEnum["sleep"] = "\uC878\uB824\uC694~";
+    ExEnum["up"] = "\uC88B\uC544\uC694";
+    ExEnum["down"] = "\uC548\uC88B\uC544\uC694";
+})(ExEnum || (ExEnum = {}));
+var what = ExEnum.up;
+console.log(ExEnum);
+console.log(what);
+exports["default"] = {};
---
+++ pr_ts.ts
@@ -0,0 +1,12 @@
+enum ExEnum {
+  sleep="졸려요~",
+  up="좋아요",
+  down="안좋아요",
+}
+
+let what = ExEnum.up
+
+console.log(ExEnum)
+console.log(what)
+
+export default {}
